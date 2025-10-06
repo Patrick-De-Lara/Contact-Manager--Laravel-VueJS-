1ef571208507e0c03c4aa7054133435548876c5c
Patrick De Lara <patrickdelara13@gmail.com>
2025-10-06 20:06:58 +0800

    debug storage on route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 // Redirect home to CrudTest module
 Route::get('/', function () {
     return redirect('/crudtests');
+});
+
+// Debug route to check storage (REMOVE IN PRODUCTION)
+Route::get('/debug/storage', function () {
+    $publicPath = public_path('storage');
+    $storagePath = storage_path('app/public');
+    
+    return [
+        'storage_link_exists' => is_link($publicPath),
+        'storage_link_target' => is_link($publicPath) ? readlink($publicPath) : null,
+        'public_path' => $publicPath,
+        'storage_path' => $storagePath,
+        'storage_files' => Storage::disk('public')->files('profile_images'),
+        'storage_disk_config' => config('filesystems.disks.public'),
+    ];
 });
