fdc4b2aa568d312185cd7351ecf61bc17f110371
Patrick De Lara <patrickdelara13@gmail.com>
2025-10-06 18:19:08 +0800

    fix vue dependency for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -11,6 +12,14 @@ export default defineConfig({
                 'Modules/CrudTest/resources/assets/js/app.js' // Add module
             ],
             refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
         tailwindcss(),
     ],
