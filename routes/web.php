05e8fd0c39c753d884d83283d47debd9bec5a76b
Patrick De Lara <patrickdelara13@gmail.com>
2025-10-06 22:28:33 +0800

    remove debug route for storage

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,3 @@ use Illuminate\Support\Facades\Storage;
 Route::get('/', function () {
     return redirect('/crudtests');
 });
-
-// Debug route to check storage (REMOVE IN PRODUCTION)
-Route::get('/debug/storage', function () {
-    $publicPath = public_path('storage');
-    $storagePath = storage_path('app/public');
-    
-    return [
-        'storage_link_exists' => is_link($publicPath),
-        'storage_link_target' => is_link($publicPath) ? readlink($publicPath) : null,
-        'public_path' => $publicPath,
-        'storage_path' => $storagePath,
-        'storage_files' => Storage::disk('public')->files('profile_images'),
-        'storage_disk_config' => config('filesystems.disks.public'),
-    ];
-});
