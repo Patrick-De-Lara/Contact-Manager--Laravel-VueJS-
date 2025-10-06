f743fbaebbb0b525f54ade3195b5455a40cdbb6a
Patrick De Lara <patrickdelara13@gmail.com>
2025-10-06 19:40:27 +0800

    fix admin panel Table choice

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,2 +1,5 @@
 {{-- This file is used for menu items by any Backpack v6 theme --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+
+{{-- Contacts CRUD --}}
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('contact') }}"><i class="la la-users nav-icon"></i> Contacts</a></li>
